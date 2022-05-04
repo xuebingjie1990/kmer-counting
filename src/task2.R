7a83be5aac226d1dfbbf60c2afd92e361b1ffd72
Bingjie Xue <xuebingjie@hotmail.com>
2022-05-04 01:50:05 -0400

    update PCA plot

--- src/task2.R
+++ src/task2.R
@@ -1,7 +1,6 @@
 library("seqinr")
-# library("ggfortify")
 library("plyr")
-library("plotly")
+library("ggfortify")
 library("ggplot2")
 
 setwd("~/Documents/Courses/BIMS8601/kmer-counting")
@@ -24,14 +23,9 @@ kmersCountMatrix = function(path){
   matrix = data.frame()
   for (f in readFile(path)){
     for (seq in loadSeq(f)){
-      seqname = attr(seq, "name")
       seq = seq[1]
-      if (!(seqname %in% row.names(matrix))){
-        counts = countKmers(seq, 4)
-        rname = rownames(matrix)
-        matrix = rbind.fill(matrix, data.frame(as.list(counts)) )
-        rownames(matrix) = c(rname, seqname)
-      }
+      counts = countKmers(seq, 4)
+      matrix = rbind.fill(matrix, data.frame(as.list(counts)) )
     }
   }
   matrix[is.na(matrix)] = 0
@@ -42,10 +36,10 @@ matrix = kmersCountMatrix("~/Documents/Courses/BIMS8601/kmer-counting/fasta/")
 pc <- prcomp(matrix,
              center = TRUE,
              scale. = TRUE)
-
-# p <- autoplot(pc)
-# ggplotly(p)
-
+# plot with ggplot2::autoplot
+p <- autoplot(pc)
+p
+# plot with ggplot
 p2 <- ggplot(pc, aes(x=PC1, y=PC2)) + geom_point()
-ggplotly(p2)
+p2
 
